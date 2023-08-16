dcaaf9190514eaf5f59b0dfb0d10968432c94cb2
akshaysi05 <akshaysingh8804@gmail.com>
2023-08-16 21:27:58 +0530

    DynamicDropDown

---
+++ UIAutomationTest/src/test/java/uiComponents/DynamicDropDown.java
@@ -0,0 +1,24 @@
+package uiComponents;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class DynamicDropDown {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+		driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
+		driver.findElement(By.xpath("//a[@value='CCU']")).click();
+		Thread.sleep(5000);
+		driver.findElement(By.xpath("(//a[@value='GOI'])[2]")).click();
+
+	}
+
+}
