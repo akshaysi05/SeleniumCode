8134056219b743434d19a6eac6bc9fb21c8195fa
akshaysi05 <akshaysingh8804@gmail.com>
2023-08-17 20:46:11 +0530

    CurrentDateDSelection

---
+++ UIAutomationTest/src/test/java/uiComponents/UIEnabledOrNot.java
@@ -0,0 +1,21 @@
+package uiComponents;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class UIEnabledOrNot {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+		driver.findElement(By.id("ctl00_mainContent_rbtnl_Trip_1")).click();
+
+	}
+
+}
