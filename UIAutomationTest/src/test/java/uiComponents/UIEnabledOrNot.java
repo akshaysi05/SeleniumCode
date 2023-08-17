4ae3b64a84a0c6d16f5e1e7402e7ec47bdfe89d9
akshaysi05 <akshaysingh8804@gmail.com>
2023-08-17 21:07:58 +0530

    UI Enabled Or Disbaled Check

--- UIAutomationTest/src/test/java/uiComponents/UIEnabledOrNot.java
+++ UIAutomationTest/src/test/java/uiComponents/UIEnabledOrNot.java
@@ -2,6 +2,7 @@ package uiComponents;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -14,8 +15,24 @@ public class UIEnabledOrNot {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+		//Check ENabled Or Disabled 
+		WebElement EnableUI= driver.findElement(By.id("ctl00_mainContent_view_date2"));
+		//System.out.println( EnableUI.isEnabled());
 		driver.findElement(By.id("ctl00_mainContent_rbtnl_Trip_1")).click();
+		//System.out.println( EnableUI.isEnabled());
+		
+		//2Methods To Check.
+		System.out.println(driver.findElement(By.id("Div1")).getAttribute("style"));
+		if(driver.findElement(By.id("Div1")).getAttribute("style").contains("1")){
+			System.out.println("It is Enabled ");
+			
+		}
+		else {
+			System.out.println("It is Disabled ");
+		}
+		
 
 	}
 
 }
+//Hare EnableUI.isEnabled() Methods Does Not Works Becaues  Ui All Time Already Enabled.
